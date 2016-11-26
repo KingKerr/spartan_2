32afdc4f2c24f46a7e70950e27cedbfaa2dee922
DKerr <drew.kerr86@gmail.com>
2016-11-25 22:06:57 -0600

    Adding whitenoise for static images

--- Grind/settings.py
+++ Grind/settings.py
@@ -122,19 +122,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join((BASE_DIR), "static")
+STATIC_ROOT = os.path.join(PROJECT_ROOT, "static")
 
-PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
+#PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
 
 MEDIA_URL = "/media/"
 
 MEDIA_ROOT = os.path.join(PROJECT_ROOT, "Spartan/static", *MEDIA_URL.strip("/").split("/"))
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "Spartan/static"),
+    os.path.join(PROJECT_ROOT, "Spartan/static"),
     #'/var/www/static/',
 ]
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = 'home'
 
